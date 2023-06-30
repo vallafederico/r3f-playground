0cc8a9b653ba6e169fd5a1fdc8395a29a22521ba
vallafederico <valla.federico@gmail.com>
2023-06-30 21:27:58 +0200

    added 02

---
+++ 02/src/components/Gl.jsx
@@ -0,0 +1,43 @@
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+// import {
+//   Bloom,
+//   DepthOfField,
+//   EffectComposer,
+//   Noise,
+//   Vignette,
+//   Grid,
+// } from "@react-three/postprocessing";
+// import { BlendFunction } from "postprocessing";
+
+import { Model } from "./model";
+
+export default function Gl() {
+  return (
+    <div className="cnv-wrap">
+      <Canvas
+        camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 0, 2] }}
+        gl={{
+          alpha: true,
+          antialias: true,
+          logarithmicDepthBuffer: true,
+          // setClearColor: "#ff0000",
+        }}
+      >
+        {/* <Plane /> */}
+
+        <Model position={[0, 0, 0]} />
+
+        <OrbitControls />
+
+        {/* <EffectComposer>
+          <Grid
+            blendFunction={BlendFunction.OVERLAY} // blend mode
+            scale={2.0} // grid pattern scale
+            lineWidth={0.0} // grid pattern line width
+          />
+        </EffectComposer> */}
+      </Canvas>
+    </div>
+  );
+}
